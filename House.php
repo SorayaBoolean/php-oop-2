7a58bb697ed5842755fa5c21fc558a3b1e70cf5c
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2023-01-15 14:23:34 +0100

    uder class and house class defined

--- House.php
+++ House.php
@@ -6,12 +6,12 @@ class house {
     public $contract;
     public $typology;
     public $floor;
-    public $squareMeters; //set it***
-    public $number_of_Rooms; //set it***
+    public $squareMeters; //set it*** //get it***
+    public $number_of_Rooms; //set it*** //get it
     public $balcony;
     public $furnished;
     public $energyClass;
-    public $price;//set it***
+    public $price;//set it*** //get it
 
 
     public function __construct($address, $contract, $typology, $floor=0, $energyClass)
@@ -24,8 +24,10 @@ class house {
         
     }
 
+    //Set Methods/////////////////////////////////////////////////////
 
-    public function setsquareMeters($squareMeters) {
+
+    public function setSquareMeters($squareMeters) {
         if (is_int($squareMeters) && $squareMeters >= 35 && $squareMeters < 2000){
             $this->squareMeters = $squareMeters;
         } else{
@@ -33,7 +35,7 @@ class house {
         }
     }
 
-    public function number_of_Rooms($number_of_Rooms) {
+    public function setNumber_of_Rooms($number_of_Rooms) {
         if (is_int($number_of_Rooms) && $number_of_Rooms >= 1 && $number_of_Rooms < 20){
             $this->number_of_Rooms = $number_of_Rooms;
         } else{
@@ -41,13 +43,27 @@ class house {
         }
     }
 
-    public function price($price) {
+    public function setPrice($price) {
         if (is_int($price) && $price >= 400 && $price < 500.000){
             $this->price = $price;
         } else{
             die();
         }
     }
+
+    //Get Methods///////////////////////////////////////////////
+
+    public function getSquareMeters() {
+        return $this->squareMeters; 
+      } 
+
+    public function getNumberOfRooms() {
+        return $this->number_of_Rooms; 
+      }
+
+    public function getPrice() {
+        return $this->price; 
+      }
 
 
 
--- Users.php
+++ Users.php
@@ -7,9 +7,16 @@
         public $email;
         public $address;
 
-        
 
 
+    public function __construct( $name , $lastname , $email , $address)
+    {
+        $this->name =$name;
+        $this->lastname =$lastname;
+        $this->email =$email;
+        $this->address =$address;
+       
+    }
 
 
     }
