9dc5d72e91bbc52dd811b927af3ce145ca2c5148
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2023-01-16 13:28:45 +0100

    added new objects for class realtor

---
+++ Realtor.php
@@ -0,0 +1,33 @@
+<?php
+
+    class Realtor extends User {
+
+        public $pIva;
+        public $agency;
+        public $telephone;
+
+        public function __construct( $name , $lastname , $email , $address, $pIva, $agency, $telephone )
+    {
+        //father attributes (estensibilità)
+        $this->name =$name;
+        $this->lastname =$lastname;
+        $this->email =$email;
+        $this->address =$address;
+
+        //paerent attributes (polimorfismo)
+        $this->pIva =$pIva;
+        $this->agency =$agency;
+        $this->telephone =$telephone;
+       
+    }
+
+    }
+
+
+
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
-/*
+
 
 require_once __DIR__ .'/House.php';
 require_once __DIR__ .'/User.php';
+require_once __DIR__ .'/Realtor.php';
 
 $user1 = new User("Soraya","Khyabani", "soraya@gmail.com","Via di Sandro Pertini");
 var_dump($user1);
 
-$user2 = new User("Manolo","Palestini", "manolo@gmail.com","Via di Sandro Pertini");
+$user2 = new Realtor("Manolo","Palestini", "manolo@gmail.com","Via di Sandro Pertini","1234567890", "Tecnocasa", "3279320478");
 var_dump($user2);
+
+$user3 = new Realtor("Gianna","Nannini", "gianna@gmail.com","Via Roma","8901234567", "Tecnocasa", "3279320668");
+var_dump($user3);
+
+
 
 $house1 = new House ("Via Sandro Pertini","Affitto", "appartamento", "first", "A");
 $house1->setSquareMeters(70);
@@ -16,6 +22,6 @@ $house1->setNumber_of_Rooms(5);
 $house1->setPrice(600);
 
 var_dump($house1);
-*/
+
 
 ?>
